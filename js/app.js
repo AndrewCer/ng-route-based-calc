b8a9fee3bd2d8b905e27a52e7371ee0662e25432
Andrew Cer <cer.andrew@gmail.com>
2015-08-21 11:37:39 -0600

    added query params to addition route

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ app.config(function ($routeProvider) {
       templateUrl: 'partials/home.html',
       controller: 'HomeController'
     })
-    .when('/add/:numone/:numtwo', {
+    .when('/add', {
       templateUrl: 'partials/add.html',
       controller: 'AddController'
     })
@@ -14,5 +14,5 @@ app.config(function ($routeProvider) {
       templateUrl: 'partials/div.html',
       controller: 'DivController'
     })
-    .otherwise({redirectTo: '/'})
+    // .otherwise({redirectTo: '/'})
 })
--- js/controller.js
+++ js/controller.js
@@ -3,14 +3,15 @@ app.controller('HomeController', function ($scope) {
 
 })
 
-app.controller('DivController', function ($scope, $routeParams) {
+app.controller('DivController', function ($scope, $routeParams, $location) {
   $scope.firstNumb = $routeParams.numone;
   $scope.secondNumb = $routeParams.numtwo;
   $scope.addedNums = Number($routeParams.numone) / Number($routeParams.numtwo)
 })
 
-app.controller('AddController', function ($scope, $routeParams) {
-  $scope.firstNumb = $routeParams.numone;
-  $scope.secondNumb = $routeParams.numtwo;
-  $scope.addedNums = Number($routeParams.numone) + Number($routeParams.numtwo)
+app.controller('AddController', function ($scope, $routeParams, $location) {
+  console.log($location.search());
+  $scope.firstNumb = $location.search().x;
+  $scope.secondNumb = $location.search().y;
+  $scope.addedNums = Number($location.search().x) + Number($location.search().y)
 })
